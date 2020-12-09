98dc41cf11b1f03d0137b5209d3b4743aefa41ff
EdisonStalin <38759862+EdisonStalin@users.noreply.github.com>
2020-12-09 12:41:23 -0500

    Ionic firebase

---
+++ GasSA/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyB7J0zC-koBR3Ep9cH0uEl4znLgDW4bqHU",
+    authDomain: "gassa-bef45.firebaseapp.com",
+    projectId: "gassa-bef45",
+    storageBucket: "gassa-bef45.appspot.com",
+    messagingSenderId: "948000982374",
+    appId: "1:948000982374:web:45ad967fe579d403e274a7"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
